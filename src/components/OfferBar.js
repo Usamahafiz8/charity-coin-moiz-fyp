74b10987caf54fab132bf5c96f17989f64703fe0
Muhammad Osama <58689276+Usamahafiz8@users.noreply.github.com>
2024-10-28 15:51:32 +0500

    responsive offer bar

--- src/components/OfferBar.js
+++ src/components/OfferBar.js
@@ -3,16 +3,18 @@ import Link from 'next/link';
 
 const OfferBar = () => {
   return (
-    <div className="bg-gray-800 text-white py-2 px-4 flex justify-between items-center">
-      <div className="flex-1">
-        <p className="text-sm">Exclusive Offers! Get 20% off on your first donation!</p>
-      </div>
+    
+    
+    <div className="bg-gray-800 ">
+      <div className='container mx-auto text-white   text-sm py-2 px-2 lg:px-0 flex justify-between items-center'> 
+        <p className="">Exclusive Offers! Get 20% off on your first donation!</p>
       <div>
         <Link href="/login">
-          <button className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-400 transition duration-200">
+          <button className="bg-green-600 text-white px-4 py-1 rounded-full hover:bg-green-400 transition duration-200">
             Login
           </button>
         </Link>
+    </div>
       </div>
     </div>
   );
