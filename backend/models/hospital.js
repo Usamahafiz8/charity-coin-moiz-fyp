df75c01be7d8065c72f7acfec019b7fc0fd40060
Muhammad Osama <58689276+Usamahafiz8@users.noreply.github.com>
2024-11-06 18:14:16 +0500

    updated the model

--- backend/models/hospital.js
+++ backend/models/hospital.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const hospitalSchema = new mongoose.Schema({
   name: { type: String, required: true },
   location: { type: String, required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true },
+  wallat_address: { type: String, required: true },
   contactNumber: { type: String, required: true },
 });
 
