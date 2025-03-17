a199eaa4fdd9e719141fd2492b3589a6a68527ec
Usamahafiz8 <usamahafiz8@gmail.com>
2025-03-17 03:29:14 -0500

    remove nodemodule from the backend and working on the frontend

---
+++ frontend/src/model/patient.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const patientSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true },
+  wallat_address: { type: String, required: true },
+  hospitalId: { type: mongoose.Schema.Types.ObjectId, ref: 'Hospital' },
+  medicalHistory: [String],
+});
+
+module.exports = mongoose.model('Patient', patientSchema);
